e1b0d792182fadbed5d07a278daa1911e50c8544
Kunwar Nir <knir45@gmail.com>
2023-01-21 13:52:55 -0500

    Revert "simple frontend"
    This reverts commit a2f8bb38c51677cc49f15d41331ed7c793ee2721.
    sorry

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import Title from './components/Title';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Title/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
