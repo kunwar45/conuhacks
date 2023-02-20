ed24a2502aeb787533ad241cb24d38a7267f2fba
Hassan Ali <thehassanali2@gmail.com>
2023-02-19 19:53:22 -0500

    Webcam fully works

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require('body-parser')
+const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
+app.use(bodyParser({limit: '50mb'}))
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/frame"));
+app.use(require("./routes/registry"));
+app.use(require("./routes/image"));
+// get driver connection
+const dbo = require("./db/conn");
+
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
+  
+  console.log(`Server is running on port: ${port}`);
+});
