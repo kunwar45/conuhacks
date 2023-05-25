a977056bb334616edec255b93f21c69950e7f039
Kunwar Nir <knir45@gmail.com>
2023-05-24 21:05:46 -0400

    components

---
+++ client/src/components/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./NavBar";
+import Content from "./Content";
+
+export default function HomePage(){
+    
+    return(
+        <>
+        <NavBar/>
+        <Content/>
+        </>
+    );    
+}
